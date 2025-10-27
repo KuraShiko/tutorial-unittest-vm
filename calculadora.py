ac55cc618a3617ecbf21eef3a8112f1ab1dbb5eb
KuraShiko <ruztigameog@gmail.com>
2025-10-26 19:54:19 -0600

    ¨test¨

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 def sumar(a, b):
     """
-    Esta función toma dos números como argumentos y devuelve su suma.
+    La función toma dos números como argumentos y devuelve su suma.
     """
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("Ambos argumentos deben ser números")
