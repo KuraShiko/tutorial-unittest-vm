35302ebcf8733f64cbf73816bd17106e397ad293
Sergio Alberto Balderrama Jr. <Ruztigameog@gmail.com>
2025-10-26 19:32:45 -0700

    Delete calculadora.py

--- calculadora.py
+++
@@ -1,7 +0,0 @@
-def sumar(a, b):
-    """
-    La función toma dos números como argumentos y devuelve su suma.
-    """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("Ambos argumentos deben ser números")
-    return a + b
